383fe0aee5e6b51ddf83ed415cfad1b2a7a91477
Victor Sanborn <victor_sanborn@live.se>
2025-01-08 23:57:15 +0100

    added cron task for adding new eur daily price

--- _extract/fetchHistoicalEURPrices.js
+++ _extract/fetchHistoicalEURPrices.js
@@ -1,23 +1,36 @@
+import { sql } from '@vercel/postgres';
 import csv from 'csv-parser';
+import dotenv from 'dotenv';
 import fs from 'fs';
+
+dotenv.config({ path: '../.env' });
 
 //Hämta ny csv här:
 // https://www.riksbank.se/sv/statistik/rantor-och-valutakurser/sok-rantor-och-valutakurser/?s=g130-SEKEURPMI&a=D&from=2018-01-02&to=2025-01-08&fs=3#result-section
 // https://www.riksbank.se/sv/statistik/rantor-och-valutakurser/sok-rantor-och-valutakurser/?s=g130-SEKEURPMI&fs=2#riksbank-seriesform
 
-// Läs CSV-filen
-fs.createReadStream('./eur_prices.csv')
-	.pipe(csv({ separator: ';', mapHeaders: ({ header }) => header.trim() })) // Ange separatorn som används i din CSV-fil
-	.on('data', (row) => {
-		// Normalisera och konvertera data
-		const datum = row['Datum'];
-		const grupp = row['Grupp'];
-		const serie = row['Serie'];
-		const värde = parseFloat(row['Värde'].replace(',', '.')); // Byt ',' till '.' och konvertera till nummer
+const prices = [];
+const ExtractEURPrices = async () => {
+	fs.createReadStream('./eur_prices.csv')
+		.pipe(csv({ separator: ';', mapHeaders: ({ header }) => header.trim() })) // Ange separatorn som används i din CSV-fil
+		.on('data', async (row) => {
+			// Normalisera och konvertera data
+			const date = row['Datum'];
+			const price = parseFloat(row['Värde'].replace(',', '.')); // Byt ',' till '.' och konvertera till nummer
 
-		// Logga resultatet
-		console.log(`Datum: ${datum}, Värde: ${värde}`);
-	})
-	.on('end', () => {
-		console.log('Bearbetning av CSV-fil är klar.');
-	});
+			prices.push({ datum: date, price });
+		})
+		.on('end', async () => {
+			// Add prices to db
+			// Logga resultatet
+			for (let i = 0; i < prices.length; i++) {
+				const date = prices[i].datum;
+				const price = prices[i].price;
+				console.log(`Datum: ${date}, Värde: ${price}`);
+				await sql`INSERT INTO euroexchange("date", eurtosekprice)
+              VALUES(${date}, ${price});`;
+			}
+		});
+};
+
+ExtractEURPrices();
---
+++ app/api/cron/dailyEuroPrice/route.js
@@ -0,0 +1,23 @@
+import { sql } from '@vercel/postgres';
+import { format } from 'date-fns';
+import { NextResponse } from 'next/server';
+import fetch from 'node-fetch';
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+export async function GET() {
+	const fetchHistoricalRate = async (date) => {
+		const url = `https://api.exchangeratesapi.io/${date}?base=EUR&symbols=SEK&access_key=10118bcfaf30a809e16001e957e25f87`;
+		const response = await fetch(url);
+		const data = await response.json();
+
+		await sql`INSERT INTO euroexchange("date", eurtosekprice)
+              VALUES(${data.date}, ${data.rates.SEK});`;
+	};
+
+	const formattedDate = format(new Date(), 'yyyy-MM-dd');
+	fetchHistoricalRate(formattedDate);
+
+	return NextResponse.json({ ok: true });
+}
