7d1e4b7e8bc0ce327406dbed23dca2b8e51d65b5
Victor Sanborn <victor.sanborn@hogia.se>
2025-01-09 17:46:43 +0100

    updated api

--- app/api/cron/dailyEuroPrice/route.js
+++ app/api/cron/dailyEuroPrice/route.js
@@ -7,17 +7,24 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 export async function GET() {
+	
 	const fetchHistoricalRate = async (date) => {
-		const url = `https://api.exchangeratesapi.io/${date}?base=EUR&symbols=SEK&access_key=10118bcfaf30a809e16001e957e25f87`;
-		const response = await fetch(url);
-		const data = await response.json();
+		try{
+			const url = `https://api.exchangeratesapi.io/${date}?base=EUR&symbols=SEK&access_key=10118bcfaf30a809e16001e957e25f87`;
+			const response = await fetch(url);
+			const data = await response.json();
 
-		let result  = await sql`INSERT INTO euroexchange("date", eurtosekprice)
-              VALUES(${data.date}, ${data.rates.SEK});`;
-			  console.log(result);
-		return NextResponse.json({ ok: true });
+			await sql`INSERT INTO euroexchange("date", eurtosekprice)
+				VALUES(${data.date}, ${data.rates.SEK});`;
+
+
+			return{ ok: true, data: `${data.date}, ${data.rates.SEK}` };
+		} catch (error) {
+			return { error: `Internal Server Error: ${error}` };
+		}	
 	};
 
 	const formattedDate = format(new Date(), 'yyyy-MM-dd');
-	fetchHistoricalRate(formattedDate);	
+	let respone = await fetchHistoricalRate(formattedDate);	
+	return NextResponse.json(respone)
 }
